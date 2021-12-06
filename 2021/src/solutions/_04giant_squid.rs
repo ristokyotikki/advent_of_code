49ea3c4e17e22702c1347090581fe8cb364a153c
Risto Kyötikki <risto.kyotikki@klinik.fi>
2021-12-07 00:03:30 +0200

    day 4 excercise 1

--- 2021/src/solutions/_04giant_squid.rs
+++ 2021/src/solutions/_04giant_squid.rs
@@ -1,7 +1,126 @@
-pub fn run() {
-  // println!("\nExcuting Day 3 Binary Diagnostics - excercise 1");
-  // excercise_one();
+use crate::solutions::utils::*;
 
-  // println!("\nExcuting Day 1 Sonar Sweep - excercise 2");
+#[derive(Debug, Clone)]
+struct BingoSheet {
+  board: [usize; 25],
+  hits: Vec<usize>,
+}
+
+impl BingoSheet {
+  fn mark(mut self, input: usize) -> Self {
+    if self.board.contains(&input) {
+      let index = self.board.iter().position(|&n| n == input).unwrap();
+      self.hits.push(index);
+    }
+
+    return BingoSheet {
+      board: self.board,
+      hits: self.hits
+    };
+  }
+
+  fn num_of_hits(&self) -> usize {
+    return self.hits.len();
+  }
+
+  fn check_bingo(&self) -> bool {
+    let pos = [0, 5, 10, 15, 20, 0, 1, 2, 3, 4];
+    for (index, start_pos) in pos.iter().enumerate() {
+      if self.hits.contains(start_pos) {
+        let mut hits = 1;
+        let mut no_match = false;
+        let mut incr = 1;
+        if index > 4 {
+          incr = 5;
+        }
+        let mut next = start_pos + incr;
+        while no_match == false {
+          if self.hits.contains(&next) {
+            hits += 1;
+            if hits == 5 {
+              return true;
+            }
+
+            next += incr;
+          } else {
+            no_match = true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+}
+
+pub fn run() {
+  println!("\nExcuting Day 4 Giant Squid - excercise 1");
+  excercise_one();
+
+  // println!("\nExcuting Day 4 Giant Squid - excercise 2");
   // excercise_two();
+}
+
+fn excercise_one() {
+  let bingo_sheets_and_draws = create_bingo_sheets_and_draws();
+  let draws = bingo_sheets_and_draws.0;
+  let mut bingo_sheets = bingo_sheets_and_draws.1.clone();
+
+  for draw in draws {
+    bingo_sheets = bingo_sheets.iter().map(|b| {
+      let cloned_board = b.clone().mark(draw);
+      if cloned_board.num_of_hits() >= 5 {
+        if cloned_board.check_bingo() {
+          calculate_bingo_answer(cloned_board, draw);
+          panic!("Found the solution");
+        }
+      }
+
+      return cloned_board;
+    }).collect();
+  }
+}
+
+fn calculate_bingo_answer(winning_sheet: BingoSheet, draw: usize) {
+  let mut total = 0;
+
+  for (i, n) in winning_sheet.board.iter().enumerate() {
+    if !winning_sheet.hits.contains(&i) {
+      total += n;
+    }
+  }
+
+  println!("Winning bingo result is {}", total * draw);
+}
+
+fn create_bingo_sheets_and_draws() -> (Vec<usize>, Vec<BingoSheet>) {
+  let lines = read_lines_from_file("src/inputs/giant_squid");
+  let mut i = 0;
+  let mut draws: Vec<usize> = Vec::new();
+  let mut bingo_sheets: Vec<BingoSheet> = Vec::new();
+  let mut new_board: [usize; 25] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ];
+
+  for (index, line) in lines.iter().enumerate() {
+    match index {
+      0 => draws = line.split(",").map(|n| n.parse::<usize>().unwrap()).collect(),
+      n if n == 1 || (n - 1) % 6 == 0 => {
+        let new_bingo_board = BingoSheet {
+          board: new_board,
+          hits: Vec::new(),
+        };
+        bingo_sheets.push(new_bingo_board);
+        i = 0;
+      }
+      _ => {
+        line.split_whitespace().for_each(|n| {
+          new_board[i] = n.parse::<usize>().unwrap();
+          i += 1;
+        });
+      }
+    }
+  }
+
+  return (draws, bingo_sheets);
 }
