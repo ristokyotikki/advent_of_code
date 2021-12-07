b7a8ea40fbb72447df8051443142c24465eb4e47
Risto Kyötikki <risto.kyotikki@klinik.fi>
2021-12-07 09:45:16 +0200

    changed input prompt slightly

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,43 +1,26 @@
 mod solutions;
 
 use solutions::*;
-use::std::fs;
 use std::io;
 
-static SOLUTIONS_PATH: &str ="./src/solutions";
-
 fn main() {
-    let paths_count = fs::read_dir(SOLUTIONS_PATH).unwrap().count() - 2;
-    let paths = fs::read_dir(SOLUTIONS_PATH).unwrap();
-    let mut choice_adjustment: isize = 1;
-
-    println!("\n\n");
-    println!("Which solution would you like to run?");
-    for (i, path) in paths.enumerate() {
-        let file_name = path.unwrap().file_name();
-        if file_name == "mod.rs" || file_name == "utils.rs" {
-            choice_adjustment -= 1;
-            continue;
-        }
-
-        let choice = i as isize + choice_adjustment;
-        println!("    {}: {:?}", choice, file_name)
-    }
-
-    let mut input = 999;
-    while input > paths_count {
-        let prompt = format!("Run solution: (Select by inputting number between: 1 and {})", paths_count);
-        input = prompt_int_input(prompt);
-    }
+    let prompt = format!(
+        "Input number of the solution you would like to execute:
+        1: Sonar Sweep
+        2: Dive!
+        3: Binary Diagnostics
+        4: Giant Squid"
+    );
+    let input = prompt_int_input(prompt);
 
     match input {
         1 => _01sonar_sweep::run(),
         2 => _02dive::run(),
         3 => _03binary_diagnostics::run(),
         4 => _04giant_squid::run(),
-        _ => panic!("input {}, not mapped to a solution", input)
+        // 5 => _04giant_squid::run(),
+        _ => panic!("Day {} not solved yet unfortunately", input),
     }
-    
 }
 
 fn prompt_int_input(prompt: String) -> usize {
@@ -45,12 +28,11 @@ fn prompt_int_input(prompt: String) -> usize {
 
     println!("\n{}", prompt);
     io::stdin()
-    .read_line(&mut input)
-    .expect("failed to read from stdin");
-    
+        .read_line(&mut input)
+        .expect("failed to read from stdin");
     let trimmed = input.trim();
     match trimmed.parse::<usize>() {
         Ok(integer) => return integer,
-        Err(error) => panic!("Failed to parse input to integer. Error: {}", error)
+        Err(error) => panic!("Failed to parse input to integer. Error: {}", error),
     }
 }
